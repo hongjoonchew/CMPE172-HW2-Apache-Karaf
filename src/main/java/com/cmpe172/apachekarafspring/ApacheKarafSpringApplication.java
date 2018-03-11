75967dc15d7b6489b56295cf6da5d156c1b3a139
hongjoonchew <hongjoon.chew@gmail.com>
2018-03-10 17:47:44 -0800

    Initialization

---
+++ src/main/java/com/cmpe172/apachekarafspring/ApacheKarafSpringApplication.java
@@ -0,0 +1,12 @@
+package com.cmpe172.apachekarafspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheKarafSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheKarafSpringApplication.class, args);
+	}
+}
